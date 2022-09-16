a828e599a48445a954951305d2f0fc5c9d076433
gearhead041 <favouradetoun@gmail.com>
2022-09-16 17:16:28 +0100

    data validation for practical create 📝

--- Dtos/PracticalDto.cs
+++ Dtos/PracticalDto.cs
@@ -7,6 +7,7 @@ public class PracticalDto
     public int Id { get; set; }
     [Required]
     public string? Name { get; set; }
+    [Range(1,3000,ErrorMessage ="Size must be greater than 0")]
     public int MaxSize { get; set; }
 
     public ICollection<StudentDto>? StudentRegistrations { get; set; }
